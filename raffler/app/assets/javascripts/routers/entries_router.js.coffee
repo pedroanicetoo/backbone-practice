d2306ca8d5c4fd6548f5b21f94cd04a781b40326
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2020-02-05 00:32:00 -0300

    start rails application with backbone

---
+++ raffler/app/assets/javascripts/routers/entries_router.js.coffee
@@ -0,0 +1,9 @@
+class Raffler.Routers.Entries extends Backbone.Router
+  routes:
+    '': 'index'
+    'entries/:id': 'show'
+
+  index: ->
+    alert "home page"
+  show: (id) ->
+      alert "Entry #{id}"
---
+++ raffler/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raffler::Application.config.secret_token = '9f445f1f8d6258934795ffebeb71e7acafdd0f39fe36af12191dbb2074885783a357d68137330541ff8f344a693442856e4904f339fecddd611f14497509fa87'
